56a5002fcf456507570ab091d5c9d688c0ee0e16
Tatsuhiko Arai <tatsu@tatsu.com>
2024-10-19 19:08:42 +0900

    Add per-user data isolation

--- src/App.tsx
+++ src/App.tsx
@@ -22,11 +22,11 @@ function App() {
       client.models.Todo.delete({ id })
   }
 
-  const { signOut } = useAuthenticator();
+  const { user, signOut } = useAuthenticator();
 
   return (
     <main>
-      <h1>My todos</h1>
+      <h1>{user?.signInDetails?.loginId}'s todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
